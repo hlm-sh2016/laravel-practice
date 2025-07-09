ad202226bf89fd5dcb2898508a2d20bcb8723f73
Specific Repo Name <specific@example.com>
2025-07-09 23:37:35 +0100

    ajout de la page article, modif app to blog

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 
 Route::view('/contact', 'contact')->name('contact');
+
+Route::view('article', 'article')->name('article');
