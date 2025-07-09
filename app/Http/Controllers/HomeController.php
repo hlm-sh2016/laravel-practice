a912668c92a31c6382b291b19c6906bfdcf56978
Specific Repo Name <specific@example.com>
2025-07-10 00:21:18 +0100

    ajout du model category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
  
-use Illuminate\Support\Facades\DB;
+use App\Models\Category;
  
 class HomeController extends Controller
 {
     public function index()
     {
-        $categories = DB::table('categories')->get();
+        $categories = Category::all(); 
         return view('home', ['categories' => $categories]);
     }
 }
