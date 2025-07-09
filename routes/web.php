2c1e4d76cae83fb75a565718d2cb505bb531c5fb
Specific Repo Name <specific@example.com>
2025-07-09 23:27:40 +0100

    optimisation des route avec name

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
+Route::view('/', 'home')->name('home');
 
 
-Route::view('/about', 'about');
+Route::view('/about', 'about')->name('about');
 
-Route::view('/contact', 'contact');
+Route::view('/contact', 'contact')->name('contact');
