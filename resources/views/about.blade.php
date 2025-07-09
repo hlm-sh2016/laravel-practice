61e1292006346d3d2731935a2f5410cd706cec37
Specific Repo Name <specific@example.com>
2025-07-09 23:22:13 +0100

    ajout du about et contact

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <H1>Base</H1>
-</body>
-</html>
+@extends('layouts.app')
+ 
+@section('content')
+    <h2 class="text-3xl font-bold">About Us</h2>
+    <p class="mt-4 text-lg text-gray-600">We're the best company in the world!</p>
+@endsection
