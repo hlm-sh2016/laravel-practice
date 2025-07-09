57ebb59bb6dbfa2550908de98cf7279bc78a54a8
Specific Repo Name <specific@example.com>
2025-07-10 00:15:59 +0100

    add homeController class

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+ 
+use Illuminate\Support\Facades\DB;
+ 
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $categories = DB::table('categories')->get();
+        return view('home', ['categories' => $categories]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\HomeController;
 
-Route::view('/', function() {
-    $categories = DB::table('categories')->get();
-    return view('home', ['categories'=> $categories]);
-
-})->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home'); 
 
 Route::view('/about', 'about')->name('about');
 
